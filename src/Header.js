113fff8dcdcb2133cd821bd733628472d6849219
oscarmendezortegaseo <122076461+oscarmendezortegaseo@users.noreply.github.com>
2023-01-06 13:19:39 -0600

    1 file modified

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import Logo from "./common/Logo"
 
 const Header = () => {
     return (
-        <div className="row" style={{background: "#171717"}}>
+        <div className="row" style={{background: "#000"}}>
             <Logo key="LogoHeaderSochi" class="col-md-6" alt="Logo Header sochi" image="img/logo-sochi.png"/>
             <Logo key="LogoHeaderClaro" class="col-md-6" alt="Logo Header claro sports" image="img/logo-claro-sports.png"/>
         </div>
