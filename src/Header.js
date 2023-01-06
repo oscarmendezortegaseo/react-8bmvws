f2167465c0ad961b94dc6a38559dd4384f014fb4
oscarmendezortegaseo <122076461+oscarmendezortegaseo@users.noreply.github.com>
2023-01-06 14:28:38 -0600

    1 file modified

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <div className="row" style={{background: "#000"}}>
             <img src={`https://cdn.jsdelivr.net/gh/oscarmendezortegaseo/react-8bmvws@main/public/img/logo-sochi.png`}/>
 
-            <Logo key="LogoHeaderSochi" class="col-md-6" alt="Logo Header sochi" image=""/>
+            <Logo key="LogoHeaderSochi" class="col-md-6" alt="Logo Header sochi" image="img/logo-sochi.png"/>
             <Logo key="LogoHeaderClaro" class="col-md-6" alt="Logo Header claro sports" image="img/logo-claro-sports.png"/>
         </div>
     );
