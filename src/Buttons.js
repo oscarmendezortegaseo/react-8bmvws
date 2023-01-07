45a21b8b2993a2c71fb7ee4918bc383800178e16
oscarmendezortegaseo <122076461+oscarmendezortegaseo@users.noreply.github.com>
2023-01-06 18:14:55 -0600

    2 files modified

--- src/Buttons.js
+++ src/Buttons.js
@@ -10,7 +10,7 @@ const Buttons = (props) => {
                 <h3 className="text-center subtitulo">
                     CLARO SPORTS EN SOCHI 2014
                 </h3>
-                <div className="control">
+                <div className="row">
                     <Button class="col-md-3 col-6" alt="logo-team-latam" image="https://cdn.jsdelivr.net/gh/oscarmendezortegaseo/react-8bmvws@main/public/img/team-latam.png" actionButton={props.changeVideo} video= {{title: "sofia", video: "qaZ0oAh4evU"}} />
                     <Button class="col-md-3 col-6" alt="logo-travel-meets-fashion" image="https://cdn.jsdelivr.net/gh/oscarmendezortegaseo/react-8bmvws@main/public/img/travel-meets-fashion.png" actionButton={props.changeVideo} video= {{title: "mike el perro", video: "MUSdiImX2M0"}} />
                     <Button class="col-md-3 col-6" alt="logo-velocidad-riesgo" image="https://cdn.jsdelivr.net/gh/oscarmendezortegaseo/react-8bmvws@main/public/img/velocidad-riesgo.png" actionButton={props.changeVideo} video= {{title: "mike fusion", video: "6Yd_jtz94DA"}} />
